19ff34928c1de823e9dcf14d475fb6988f11cf95
Jeshurun Hembd <jjhembd@gmail.com>
2021-07-02 13:46:00 -0500

    Update dependencies, use MapLibre references in README

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,5 +1,5 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs'; // Needed for pbf
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'; // Needed for pbf
 import pkg from "../package.json";
 
 export default {
